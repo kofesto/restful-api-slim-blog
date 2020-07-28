a42aafe143aefb39ff1f28ba41cb2bb7350f53bc
koffi seglo <kofestotech@gmail.com>
2020-07-28 08:57:53 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -16,6 +16,26 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // getting instance of app
 $app = (new Marcosricardoss\Restful\App(__DIR__.'/../'))->get();
+//Adding CORS
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+            ->withHeader('Access-Control-Allow-Credentials', 'true');
+});
+
+// Catch-all route to serve a 404 Not Found page if none of the routes match
+// NOTE: make sure this route is defined last
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
+});
 
 // Run app
 $app->run();
